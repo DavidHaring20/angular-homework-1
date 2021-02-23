1ee319752512bd7918bf2aa42cbc689ce2648dc4
David <davidharingri@gmail.com>
2021-02-23 11:19:26 +0100

    Worked on TO DO app. Interact with animation, data binding and interopolation.

--- angular-homework1/src/app/app.module.ts
+++ angular-homework1/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
@@ -12,7 +14,9 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
